62032470f8405bd409604a1f559705cec142f9cb
FVSSANTOS <flaviovss@yahoo.com>
2023-04-27 14:35:28 -0300

    .

--- jdbc/src/application/Program.java
+++ jdbc/src/application/Program.java
@@ -9,17 +9,55 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import db.DB;
+import db.DbException;
 import db.DbIntegrityException;
 
 public class Program {
     public static void main(String[] args) throws Exception {
 
+
+        //Transações
+        Connection conn = null;
+        Statement st = null;
+        try{
+            conn = DB.getConnection();
+
+            conn.setAutoCommit(false);
+
+            st = conn.createStatement();
+
+            int rowns1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+
+            int rowns2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+            
+            conn.commit();
+
+            System.out.println("rowns1"+ rowns1);
+            System.out.println("rowns2"+rowns2);
+        }
+        catch(SQLException e){
+            try {
+                conn.rollback();
+                throw new DbException("Transaction roller back! Caused by:"+ e.getMessage());               
+            } catch (SQLException el) {
+                throw new DbException("Error trying to rollback! Cause by:"+el.getMessage());
+            }
+        }
+        finally{
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+
+
+
         //Deletar dados
+        /*
         Connection conn = null;
         PreparedStatement st = null;
         try{
             conn = DB.getConnection();
-
+            
             st = conn.prepareStatement(
                 "DELETE FROM department"
                 +"WHERE "
@@ -46,30 +84,31 @@ public class Program {
         PreparedStatement st = null;
         try{
             conn = DB.getConnection();
-
+            
             st = conn.prepareStatement(
                 "UPDATE seller "
                 +"SET BaseSalary = BaseSalary + ? "
                 +"WHERE "
                 +"DepartmentId = ?"
-            );
-
-            st.setDouble(1, 200.0);
-            st.setInt(2, 2);
-            
-            int rowsAffected = st.executeUpdate();
-            
-            System.out.println("Done! Rows affected: "+rowsAffected);
-        }
-        catch(SQLException e){
-            e.printStackTrace();
-        }
-        finally{
-            DB.closeStatement(st);
-            DB.closeConnection();
-        }
-        */
+                );
+                
+                st.setDouble(1, 200.0);
+                st.setInt(2, 2);
+                
+                int rowsAffected = st.executeUpdate();
+                
+                System.out.println("Done! Rows affected: "+rowsAffected);
+            }
+            catch(SQLException e){
+                e.printStackTrace();
+            }
+            finally{
+                DB.closeStatement(st);
+                DB.closeConnection();
+            }
+            */
         
+            
         //Inserir dados
         /*     
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
--- jdbc/src/db/DB.java
+++ jdbc/src/db/DB.java
@@ -4,7 +4,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-<<<<<<< HEAD
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,19 +11,11 @@ import java.util.Properties;
 
 public class DB {
 
-=======
-import java.sql.SQLException;
-import java.util.Properties;
-
-public class DB {
-    
->>>>>>> 6a57770123dfb98647d72bc7ec05d0686ba64b83
     private static Connection conn = null;
 
     public static Connection getConnection(){
         if(conn == null){
             try{
-<<<<<<< HEAD
             Properties props = loadProperties();
             String url = props.getProperty("dburl");
             conn = DriverManager.getConnection(url, props);
@@ -33,16 +24,6 @@ public class DB {
              throw new DbException(e.getMessage());
             }
         } 
-=======
-                Properties props = loadProperties();
-                String url = props.getProperty("dburl");
-                conn = DriverManager.getConnection(url, props);
-            }
-             catch(SQLException e){
-                throw new DbException(e.getMessage());
-             }   
-        }
->>>>>>> 6a57770123dfb98647d72bc7ec05d0686ba64b83
         return conn;
     }
 
@@ -56,26 +37,17 @@ public class DB {
             }
         }
     }
-<<<<<<< HEAD
     
-=======
-
->>>>>>> 6a57770123dfb98647d72bc7ec05d0686ba64b83
     private static Properties loadProperties(){
         try(FileInputStream fs = new FileInputStream("db.properties")){
             Properties props = new Properties();
             props.load(fs);
             return props;
-<<<<<<< HEAD
         }
-=======
-        } 
->>>>>>> 6a57770123dfb98647d72bc7ec05d0686ba64b83
         catch(IOException e){
             throw new DbException(e.getMessage());
         }
     }
-<<<<<<< HEAD
 
     public static void closeStatement(Statement st){
         if(st != null){
@@ -96,6 +68,4 @@ public class DB {
             }
         }
     }
-=======
->>>>>>> 6a57770123dfb98647d72bc7ec05d0686ba64b83
 }
